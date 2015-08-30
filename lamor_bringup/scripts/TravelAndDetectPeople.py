e6ba257ac53059d2bb52159951ab1d3c7d6766ba
Stephanie <stephanie.lowry@oru.se>
2015-08-30 17:07:16 +0100

    [lamor_bringup] adding linda_wander_routine and adding in repeating tasks

--- lamor_bringup/scripts/TravelAndDetectPeople.py
+++ lamor_bringup/scripts/TravelAndDetectPeople.py
@@ -2,6 +2,7 @@
 
 import rospy
 
+from std_msgs.msg import String
 from strands_executive_msgs import task_utils
 from strands_executive_msgs.msg import Task
 from strands_navigation_msgs.msg import TopologicalMap
@@ -21,11 +22,17 @@ class TravelAndDetectPeople(RobotRoutine):
 
     def __init__(self, daily_start, daily_end, idle_duration=rospy.Duration(5), charging_point = 'ChargingPoint'):     
         RobotRoutine.__init__(self, daily_start, daily_end, idle_duration=idle_duration, charging_point=charging_point)
-        self.node_names = set()        
+        self.node_names = set()  
+	self.current_node = None
+	self.closest_node = None      
         self.topological_map = None
-        rospy.Subscriber('topological_map', TopologicalMap, self.map_callback)
+	self.current_node_msg = None
+	self.closest_node_msg = None
         self.random_nodes = []
-
+        rospy.Subscriber('topological_map', TopologicalMap, self.map_callback)
+	rospy.Subscriber('current_node', String, self.current_node_callback)
+	rospy.Subscriber('closest_node', String, self.closest_node_callback)
+   
     def map_callback(self, msg):        
         print 'got map: %s' % len(msg.nodes)
         self.topological_map = msg
@@ -33,12 +40,31 @@ class TravelAndDetectPeople(RobotRoutine):
         if len(self.random_nodes) == 0:
             self.random_nodes = list(self.node_names)
 
+    def current_node_callback(self,msg):
+	self.current_node_msg = msg
+	self.current_node = msg.data
+
+    def closest_node_callback(self,msg):
+	self.closest_node_msg = msg
+	self.closest_node = msg.data	
+
     def get_nodes(self):
         while len(self.node_names) == 0:
             print 'no nodes'
             rospy.sleep(1)
         return self.node_names
 
+    def get_current_node(self):
+        return self.current_node
+
+    def get_closest_node(self):
+        return self.closest_node
+
+    def get_best_node(self):
+	cn = self.get_current_node()
+        if not cn:
+		return self.get_closest_node()
+	return self.get_current_node()
 
     def all_waypoints(self):
         return self.get_nodes()
@@ -62,15 +88,10 @@ class TravelAndDetectPeople(RobotRoutine):
         return max_time
 
     def create_detection_routine(self, waypoints=None, daily_start=None, daily_end=None, repeat_delta=None):
-	pass
-	# try to detect person
-	# if person detected
-	# go to person
-	# look at person
-	# what else
 
-        #tasks = self.create_interaction_task()
-        #self.create_task_routine(tasks=[tasks], daily_start=daily_start, daily_end=daily_end, repeat_delta=repeat_delta)
+        tasks = self.create_interaction_task()
+	repeat_delta = timedelta(minutes=5)
+        self.create_task_routine(tasks=[tasks], daily_start=daily_start, daily_end=daily_end, repeat_delta=repeat_delta)
 
     def create_routine(self):
         
@@ -81,54 +102,51 @@ class TravelAndDetectPeople(RobotRoutine):
             Called when the routine is idle. Default is to trigger travel to the charging. As idleness is determined by the current schedule, if this call doesn't utlimately cause a task schedule to be generated this will be called repeatedly.
         """
 
-        if not waypoints: 
-            waypoints = self.get_nodes()
-
-	self.find_nearby_nodes(waypoints)
-        if not isinstance(self.random_nodes, list):
-            self.random_nodes = list(self.random_nodes)
+        nearby_nodes = self.find_nearby_nodes()
 
         rospy.loginfo('Idle for too long, generating a random waypoint task')
-        self.add_tasks([self.create_travel_task(random.choice(self.random_nodes))])
+        self.add_tasks([self.create_travel_task(random.choice(nearby_nodes))])
     
     def create_travel_task(self, waypoint_name, max_duration=rospy.Duration(60)):
 	# Task: robot travels to waypoint
 	# 
 	task = Task()
-	task.action = '/flexbe/execute_behavior'
-	task_utils.add_string_argument(task,'Tell Random Joke')
-	task.max_duration = max_duration
-	task.start_node_id = waypoint_name
-	task.end_node_id = waypoint_name
-        return task
-
-    def create_interaction_task(self, waypoint_name, max_duration=rospy.Duration(30)):
-	# Task: robot detects human and interacts
-	# 
-	task = Task()
 	#task.action = '/flexbe/execute_behavior'
-	#task_utils.add_string_argument(task,'Example Behavior')
+	#task_utils.add_string_argument(task,'Tell Random Joke')
 	task.max_duration = max_duration
 	task.start_node_id = waypoint_name
 	task.end_node_id = waypoint_name
         return task
 
-    def find_nearby_nodes(self, waypoints):
-	# need curpos
-	# need num
-	currpos = self.closest_node()
-	print currpos
-	return
-	num = 5
+    def create_interaction_task(self, max_duration=rospy.Duration(30)):
+	# Task: robot detects human and interacts
+	task = Task()
+	task.action = '/flexbe/execute_behavior'
+	task_utils.add_string_argument(task,'Example Behavior')
+	task.max_duration = max_duration
+	#task.start_node_id = waypoint_name
+	#task.end_node_id = waypoint_name
+        return task
+
+    def find_nearby_nodes(self, waypoints=None):
+
+        if not waypoints: 
+            waypoints = self.get_nodes()
+
+	# need currpos
+	currpos = self.get_best_node()
+
         expected_time = rospy.ServiceProxy('topological_navigation/travel_time_estimator', EstimateTravelTime)        
 
+	num = 10
         times = []
+
         for node in waypoints:
-		if start != currpos:
+		if node != currpos and node != 'ChargingPoint' and node != 'Station':
 		    et = expected_time(start=currpos, target=node).travel_time
-		    times.append(et)
+		    times.append((et.to_sec(),node))
 
 	times.sort()
-	nearby_times = times[:num]
-        return nearby_times
+	nearby_nodes =  [seq[1] for seq in times[:num] ]
+        return nearby_nodes
 
---
+++ lamor_bringup/scripts/linda_wander_routine.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import rospy
+from TravelAndDetectPeople import TravelAndDetectPeople
+
+from datetime import time, date, timedelta
+from dateutil.tz import tzlocal
+
+from strands_executive_msgs.msg import Task
+from strands_executive_msgs import task_utils
+
+if __name__ == '__main__':
+
+    rospy.init_node('linda_wander_routine')
+
+    # start and end times -- all times should be in local timezone
+    localtz = tzlocal()
+    start = time(8,00, tzinfo=localtz)
+    end = time(20,00, tzinfo=localtz)
+
+    # how long to stand idle before doing something
+    idle_duration=rospy.Duration(20)
+
+    routine = TravelAndDetectPeople(daily_start=start, daily_end=end, idle_duration=idle_duration) 
+
+    routine.create_routine()
+
+    routine.start_routine()
+
+    rospy.spin()
