61ca433cb6d86988e08b8f88a15c36414c2c0f7f
tayyab-naseer <naseer@cs.uni-freiburg.de>
2015-08-31 18:30:34 +0100

    fixing return from detect face state

--- lamor_flexbe_states/src/lamor_flexbe_states/detect_face_state.py
+++ lamor_flexbe_states/src/lamor_flexbe_states/detect_face_state.py
@@ -24,8 +24,7 @@ import sys
 from sensor_msgs.msg import PointCloud2
 
 
-
-
+face_detected= False
 class DetectFaceState(EventState):
     '''
     Displays the picture to the local home folder.
@@ -33,22 +32,27 @@ class DetectFaceState(EventState):
     ># Image    Image       The received Image
     <= done                 Displaying the Picture
     '''
-
+    
     def __init__(self):
         super(DetectFaceState, self).__init__(outcomes = ['face_is_detected','face_is_not_detected'],    input_keys = ['Image'])
                 
 
     def execute(self, userdata):
-        return 'done'
+        global face_detected
+        if (face_detected == True) : 
+            return 'face_is_detected'
+        else:
+            return 'face_is_not_detected'
 
     def on_enter(self,userdata):
+        global face_detected
         bridge =  CvBridge()
         cv_image = bridge.imgmsg_to_cv2(userdata.Image, desired_encoding="passthrough")
         faceCascade = cv2.CascadeClassifier(os.path.expanduser('~/haarcascade_frontalface_default.xml'))
         faces = faceCascade.detectMultiScale(cv2.cvtColor(cv_image,cv2.COLOR_BGR2GRAY),scaleFactor=1.1,minNeighbors=5,minSize=(30, 30),flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
         if (len(faces)>=1):
             print 'I have detected face' 
-            return 'face_is_detected'
+            face_detected = True
         else:
             print 'No face was detected'
-            return 'face_is_not_detected'
+            face_detected = False
